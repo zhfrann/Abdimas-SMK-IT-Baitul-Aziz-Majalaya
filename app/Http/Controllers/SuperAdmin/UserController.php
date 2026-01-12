cd61358afa92476a5e660e9f2f8cd4fd2d05856e
muhzhafran <muhzhafranilham13@gmail.com>
2026-01-12 12:41:05 +0700

    feat: menambah feedback message di form add user

--- app/Http/Controllers/SuperAdmin/UserController.php
+++ app/Http/Controllers/SuperAdmin/UserController.php
@@ -26,7 +26,6 @@ class UserController extends Controller
     {
         $request->validate([
             'name' => 'required|string|max:255',
-            // 'email' => 'required|email|unique:users',
             'password' => 'required|string|min:6|confirmed',
             'role' => 'required|exists:roles,name',
             'username' => 'required|string|max:255|unique:users'
